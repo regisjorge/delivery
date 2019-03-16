bea4b50a2f63cbcf39c0380220b8d2aca747017d
Regis G. Jorge <Regis G. Jorge@DESKTOP-U2L3UUU.home>
2019-03-16 20:36:40 -0300

    Codificado Oauth2

--- delivery-auth-server/src/main/java/com/codref/delivery/DeliveryAuthServerApplication.java
+++ delivery-auth-server/src/main/java/com/codref/delivery/DeliveryAuthServerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DeliveryAuthServerApplication {
 
 	public static void main(String[] args) {
+		// http://localhost:9091/
 		SpringApplication.run(DeliveryAuthServerApplication.class, args);
 	}
 
