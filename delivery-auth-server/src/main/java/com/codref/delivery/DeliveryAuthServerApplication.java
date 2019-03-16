96503f0a093ba03174f88d708bfe21d1256c778f
regisjorge <regis.jorge@hotmail.com>
2019-03-16 09:31:06 -0300

    Projeto Oauth2

---
+++ delivery-auth-server/src/main/java/com/codref/delivery/DeliveryAuthServerApplication.java
@@ -0,0 +1,13 @@
+package com.codref.delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryAuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryAuthServerApplication.class, args);
+	}
+
+}
